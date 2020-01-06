b447dac57444b3e862fe389682a3a685067510ee
Igor Ryzhov <vinokur141@gmail.com>
2020-01-06 15:15:53 +0500

    fix Link typings

--- src/lib/effector-react-router/match-url.ts
+++ src/lib/effector-react-router/match-url.ts
@@ -1,16 +1,41 @@
 import { pathToRegexp } from 'path-to-regexp';
 
-export const matchUrl = (pathname: string, url: string) => {
-  const keys: any = [];
+type MatchUrl = {
+  pathname: string,
+  url: string,
+}
+
+type MatchUrlResult = { [key: string]: string } | null
+
+export const matchUrl = ({ pathname, url }: MatchUrl): MatchUrlResult => {
+  const keys: {
+    name: string,
+    prefix: string,
+    suffix: string,
+    pattern: string,
+    modifier: string,
+  }[] = [];
+
   const pattern = pathToRegexp(pathname, keys);
   const match = pattern.exec(url);
-  if (!match) return;
-  const params = Object.create(null);
-  for (let i = 1; i < match.length; i += 1) {
-    params[keys[i - 1].name] = match[i] !== undefined ? match[i] : undefined;
+
+  if (!match) return null;
+
+  let params: { [key: string]: string } = {};
+
+  for (let key = 1; key < match.length; key++) {
+    if (match[key]) {
+      params = {
+        ...params,
+        [keys[key - 1].name]: match[key],
+      }
+    }
   }
-  return {
-    pathname,
-    params
-  };
+
+  return params;
 };
+
+matchUrl({
+  pathname: '/article/:id/:type/:anything',
+  url: '/article/1488/type-dev/anything-dimgo'
+});
